98f7108f929c7979a488f204283242b67f7c7dae
sungmini.kim <clever1501@naver.com>
2025-07-02 16:01:15 +0900

    [feature] twice different / part score

--- Project21/SimilarityChecker.cpp
+++ Project21/SimilarityChecker.cpp
@@ -11,8 +11,13 @@ public:
 		int length2 = str2.length();
 
 		if (length1 == length2) return 60;
-		
-		return 0;
+
+		if (length1 / 2 >= length2 || length2 / 2 >= length1) return 0;
+
+		int shorter = std::min(length1, length2);
+		int gap = std::abs(length1 - length2);
+
+		return 60 * (1 - (double)gap / shorter);
 	}
 
 };
--- Project21/SimilarityChecker_test.cpp
+++ Project21/SimilarityChecker_test.cpp
@@ -9,10 +9,48 @@ TEST(SimilarityCheckerTest, SameStringNum) {
 
 	SimilarityChecker similarityChecker;
 
-	string str1 = "1234567890";
-	string str2 = "1234567890";
+	string str1 = "ABCDE";
+	string str2 = "ABCDE";
 
 	int ret = similarityChecker.checkLengthSimilarity(str1, str2);
 
 	EXPECT_EQ(ret, 60); 
 }
+
+TEST(SimilarityCheckerTest, TwiceLengthString) {
+
+	SimilarityChecker similarityChecker;
+
+	string str1 = "ABCDE";
+	string str2 = "ABCDEABCDE";
+
+	int ret = similarityChecker.checkLengthSimilarity(str1, str2);
+
+	EXPECT_EQ(ret, 0);
+}
+
+TEST(SimilarityCheckerTest, TwiceLengthString2) {
+
+	SimilarityChecker similarityChecker;
+
+	string str1 = "ABCDEABCDE";
+	string str2 = "ABCDE";
+
+	int ret = similarityChecker.checkLengthSimilarity(str1, str2);
+
+	EXPECT_EQ(ret, 0);
+}
+
+TEST(SimilarityCheckerTest, PartScoreStringNum) {
+
+	SimilarityChecker similarityChecker;
+
+	string str1 = "ABCDEAB";
+	string str2 = "ABCDE";
+
+	int ret = similarityChecker.checkLengthSimilarity(str1, str2);
+
+	EXPECT_EQ(ret, 36);
+}
+
+
