3973829501c88263a959429e806a2d2dc574b151
sungmini.kim <clever1501@naver.com>
2025-07-02 15:46:15 +0900

    [feature] same string

--- Project21/SimilarityChecker.cpp
+++ Project21/SimilarityChecker.cpp
@@ -1,5 +1,18 @@
+#include <string>
 
+using std::string;
 
 class SimilarityChecker {
+
+public:
+	int checkLengthSimilarity(const std::string& str1, const std::string& str2) {
+		
+		int length1 = str1.length();
+		int length2 = str2.length();
+
+		if (length1 == length2) return 60;
+		
+		return 0;
+	}
 
 };
--- Project21/SimilarityChecker_test.cpp
+++ Project21/SimilarityChecker_test.cpp
@@ -1,7 +1,18 @@
 #include "gmock/gmock.h"
+#include "SimilarityChecker.cpp"
+#include <string>
 
 using namespace testing;
+using std::string;
 
-TEST(SimilarityCheckerTest, BasicAssertions) {
-	EXPECT_EQ(1, 1); 
+TEST(SimilarityCheckerTest, SameStringNum) {
+
+	SimilarityChecker similarityChecker;
+
+	string str1 = "1234567890";
+	string str2 = "1234567890";
+
+	int ret = similarityChecker.checkLengthSimilarity(str1, str2);
+
+	EXPECT_EQ(ret, 60); 
 }
